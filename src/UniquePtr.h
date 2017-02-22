9285ad602219995584e4eb91c49c9f1e0cc68de1
dixxi <dixxi1@gmx.net>
2017-02-22 19:15:08 +0100

    fixed make_unique

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -22,11 +22,11 @@
 namespace tigl
 {
 #ifdef HAVE_CPP11
-    template <T>
+    template <template T>
     using unique_ptr = std::unique_ptr<T>;
 
     template<typename T, typename... Args>
-    auto make_unique(Args&& args) -> unique_ptr<T>
+    auto make_unique(Args&&... args) -> unique_ptr<T>
     {
         return unique_ptr<T>(new T(std::forward<Args>(args)...));
     }
@@ -63,19 +63,19 @@ namespace tigl
     }
 
     template <typename T, typename Arg1>
-    unique_ptr<T> make_unique(const Arg1& arg1)
+    unique_ptr<T> make_unique(Arg1& arg1)
     {
         return unique_ptr<T>(new T(arg1));
     }
 
     template <typename T, typename Arg1, typename Arg2>
-    unique_ptr<T> make_unique(const Arg1& arg1, const Arg2& arg2)
+    unique_ptr<T> make_unique(Arg1& arg1, Arg2& arg2)
     {
         return unique_ptr<T>(new T(arg1, arg2));
     }
 
     template <typename T, typename Arg1, typename Arg2, typename Arg3>
-    unique_ptr<T> make_unique(const Arg1& arg1, const Arg2& arg2, const Arg3& arg3)
+    unique_ptr<T> make_unique(Arg1& arg1, Arg2& arg2, Arg3& arg3)
     {
         return unique_ptr<T>(new T(arg1, arg2, arg3));
     }
